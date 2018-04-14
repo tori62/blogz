63d99d989f12be92f39724e3db3120a2a3024225
Tori Gonzalez <tori@nexmatix.com>
2018-04-14 14:48:52 -0500

    got the forms to post to same website and add to database

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'secretpassword'
 
-@app.route('/newpost', methods=['POST','GET'])
-def new_blog_post():
-    return render_template('newpost.html')
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.String(1000))
+
+    def __init__(self,title,body):
+        self.title = title
+        self.body = body
+
+@app.route('/', methods=['POST','GET'])
+def new_blog_entry():
+    if request.method == 'POST':
+        new_title = request.form['blog-title']
+        new_post = request.form['body']
+
+        new_entry = Blog(new_title,new_post)
+        db.session.add(new_entry)
+        db.session.commit()
+        return render_template('newpost.html',blog_title=new_title,body=new_post)
+    else:
+        return render_template('newpost.html')
 
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run()   
