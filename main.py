904a55e6f4880ddf085cf00a468c81517aa3e4d8
Tori Gonzalez <tori@nexmatix.com>
2018-04-18 09:24:41 -0500

    changed the database connection command to new SQL database blobz

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://build-a-blog:assign3@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://blogz:assign4@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'secretpassword'
