6790f7aa322936f83a2ff297b025e4a10b849137
Tori Gonzalez <tori@nexmatix.com>
2018-04-19 17:33:13 -0500

    Registeration and Login page working.  Need to work on posting blod and linking id

--- main.py
+++ main.py
@@ -14,22 +14,98 @@ class Blog(db.Model):
     body = db.Column(db.String(1000))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self,title,body,author_id):
+    def __init__(self,title,body,author):
         self.title = title
         self.body = body
-        self.author_id = author_id
+        self.author_id = author
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
-    blogs = db.relationship('Blog', backref='author_id')
+    blogs = db.relationship('Blog', backref='author')
 
     def __init__(self,username,password):
         self.username = username
         self.password = password
 
+# This route allows a writer to signup for access to post a blog.  
+
+@app.route("/signup", methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if not_valid(username):
+            error = "Please enter a valid username (between 3-20 characters and no spaces.)"
+            username = ''
+            password = ''
+            return render_template('signup.html',username = username, password = password, name_error=error)
+        else:
+            pass
+
+        if password == '':
+            error = "Please enter a valid password (between 3-20 characters and no spaces.)"
+            return render_template('signup.html', username=username, password = password, word_error=error)
+
+        if not_valid(password):
+            error = "Please enter a valid password (between 3-20 characters and no spaces.)"
+            password = ''
+            return render_template('signup.html', username=username, password = password, word_error=error)
+        else:
+            pass
+
+        if verify != password:
+            error = "Password did not match. Please re-enter password and verify."
+            password = ''
+            return render_template('signup.html',username=username,verify_error = error )
+        else:
+            pass
+
+        existing_author = User.query.filter_by(username=username).first()
+        if not existing_author:
+            new_user = User(username,password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/blog')
+        else:
+            error = "That username already exists.  Please enter another username."
+            password = ''
+            return render_template('signup.html',username = username, password = password, name_error=error)
+    else:
+        return render_template('signup.html')
+
+
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify_password']
+        user = User.query.filter_by(username=username).first()
+
+        if password == '':
+            error = "Please enter password."
+            return render_template('login.html', username=username, password = password, word_error=error)
+
+        if verify != password:
+            error = "Password did not match. Please re-enter password and verify."
+            password = ''
+            return render_template('login.html',username=username,verify_error = error )
+        elif not user:
+            username = ''
+            error = "Username does not exist"
+            return render_template('login.html', username=username, name_error = error)
+        else:
+            return redirect('/newpost')
+
+            
+    return render_template('login.html')
 
 
 @app.route('/newpost', methods=['POST','GET'])
@@ -37,7 +113,8 @@ def new_blog_entry():
     if request.method == 'POST':
         new_title = request.form['blog-title']
         new_post = request.form['body']
-        author_id = request.form['author-id']
+        user = User.query.all()
+        author_id = user.id
 
         if new_title == "" or new_post == "":
             error = "Please enter additional information."
@@ -53,53 +130,7 @@ def new_blog_entry():
     else:
         return render_template('newpost.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
-@app.route("/signup", methods = ['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        verify = request.form['verify']
-
-        if username == "":
-            error = "Please enter a username."
-            return render_template('signup.html',name_error=error)
-        elif not_valid(username) == True:
-            error = "Please enter a valid username (between 3-20 characters and no spaces.)"
-            username = ''
-            return render_template('signup.html',username = username, name_error = error)
-        else:
-            pass
-
-        if password == "":
-            error = "Please enter a password."
-            return render_template('signup.html', username=username, word_error=error)
-        elif not_valid(password) == True:
-            error = "Please enter a valid username (between 3-20 characters and no spaces.)"
-            password = ''
-            return render_template('signup.html', username=username, password = password, word_error=error)
-        else:
-            pass
-
-        if verify != password:
-            error = "Password did not match. Please re-enter password and verify."
-            password = ''
-            return render_template('signup.html',username=username,verify_error = error )
-        else:
-            pass
-
-        new_user = User(username,password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        session['user'] = new_user.username
-
-        return redirect('/blog')
-    else:
-        return render_template('signup.html')
 
 @app.route('/blog')
 def blog_post():
@@ -122,6 +153,7 @@ def not_valid(info):
             return True
         else:
             return False
+   
 
 if __name__ == '__main__':
     app.run()   
