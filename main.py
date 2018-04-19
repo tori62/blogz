a3663f2a8e42e0b27cabd371f7670e4a32f38a0e
Tori Gonzalez <tori@nexmatix.com>
2018-04-19 08:58:17 -0500

    Completed signup templates and routes

--- main.py
+++ main.py
@@ -12,23 +12,39 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     body = db.Column(db.String(1000))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self,title,body):
+    def __init__(self,title,body,author_id):
         self.title = title
         self.body = body
+        self.author_id = author_id
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='author_id')
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+
 
 @app.route('/newpost', methods=['POST','GET'])
 def new_blog_entry():
     if request.method == 'POST':
         new_title = request.form['blog-title']
         new_post = request.form['body']
+        author_id = request.form['author-id']
 
         if new_title == "" or new_post == "":
             error = "Please enter additional information."
             return render_template('newpost.html',blog_title=new_title,body=new_post,error=error)
 
         else:
-            new_entry = Blog(new_title,new_post)
+            new_entry = Blog(new_title,new_post,author_id)
             db.session.add(new_entry)
             db.session.commit()
 #        return render_template('single.html',blog_title=new_title,body=new_post)
@@ -36,6 +52,54 @@ def new_blog_entry():
 
     else:
         return render_template('newpost.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route("/signup", methods = ['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if username == "":
+            error = "Please enter a username."
+            return render_template('signup.html',name_error=error)
+        elif not_valid(username) == True:
+            error = "Please enter a valid username (between 3-20 characters and no spaces.)"
+            username = ''
+            return render_template('signup.html',username = username, name_error = error)
+        else:
+            pass
+
+        if password == "":
+            error = "Please enter a password."
+            return render_template('signup.html', username=username, word_error=error)
+        elif not_valid(password) == True:
+            error = "Please enter a valid username (between 3-20 characters and no spaces.)"
+            password = ''
+            return render_template('signup.html', username=username, password = password, word_error=error)
+        else:
+            pass
+
+        if verify != password:
+            error = "Password did not match. Please re-enter password and verify."
+            password = ''
+            return render_template('signup.html',username=username,verify_error = error )
+        else:
+            pass
+
+        new_user = User(username,password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user'] = new_user.username
+
+        return redirect('/blog')
+    else:
+        return render_template('signup.html')
 
 @app.route('/blog')
 def blog_post():
@@ -49,6 +113,15 @@ def blog_post():
         post = blogs.body
         return render_template('single.html',blog_title=title,body=post)
 
+
+def not_valid(info):
+    for i in info:
+        if i == " ":
+            return True
+        elif len(info) < 3 or len(info) > 20:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     app.run()   
